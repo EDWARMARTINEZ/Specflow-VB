877bdacdf2816d185f73b94ecca3abe616f91800
EDWARMARTINEZ <edwar.martinez@udea.edu.co>
2018-10-28 14:22:22 -0500

    1. Refactorizacion proyecto

--- Ejemplo/Calculator.cs
+++ Ejemplo/Calculator.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Ejemplo
 {
+    [TestClass]
     public class Calculator
     {
         public int FirstNumber { get; set; }
@@ -15,6 +13,6 @@ namespace Ejemplo
         {
             return FirstNumber + SecondNumber;
         }
-
     }
+   
 }
--- Ejemplo/Properties/AssemblyInfo.cs
+++ Ejemplo/Properties/AssemblyInfo.cs
@@ -1,10 +1,7 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// La información general de un ensamblado se controla mediante el siguiente 
-// conjunto de atributos. Cambie estos valores de atributo para modificar la información
-// asociada con un ensamblado.
 [assembly: AssemblyTitle("Ejemplo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,23 +11,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
-// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
-// COM, establezca el atributo ComVisible en true en este tipo.
 [assembly: ComVisible(false)]
 
-// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("31c63e19-6864-47f7-88b8-7960d252a601")]
+[assembly: Guid("fdd5f8fd-6efd-4af2-8d17-0917b6ecaf6a")]
 
-// La información de versión de un ensamblado consta de los cuatro valores siguientes:
-//
-//      Versión principal
-//      Versión secundaria
-//      Número de compilación
-//      Revisión
-//
-// Puede especificar todos los valores o usar los números de compilación y de revisión predeterminados
-// mediante el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
